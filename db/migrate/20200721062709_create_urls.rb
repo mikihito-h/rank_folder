c0389ddf48236100f3f3e2d812b8fb1d9bb32176
horikoshi <mkhth3a@gmail.com>
2020-07-21 15:37:13 +0900

    urlsテーブルのurlカラムにnot null制約を追加

--- db/migrate/20200721062709_create_urls.rb
+++ db/migrate/20200721062709_create_urls.rb
@@ -1,7 +1,7 @@
 class CreateUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :urls do |t|
-      t.string :url
+      t.string :url, null: false
       t.integer :user_id
 
       t.timestamps
