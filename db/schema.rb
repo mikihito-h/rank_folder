5a89c5389327bbf6da14f1f0f5cf4050cfc6aa72
horikoshi <mkhth3a@gmail.com>
2020-08-04 13:19:57 +0900

    アカウント登録した時にAPIキーが生成されるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_064022) do
+ActiveRecord::Schema.define(version: 2020_08_04_030203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_08_02_064022) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "api_key"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
