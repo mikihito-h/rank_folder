2ee768f68ca2fbb3580a7e71dbd6f043a5cc3977
horikoshi <mkhth3a@gmail.com>
2020-08-04 14:14:29 +0900

    クエリパラメータにAPIキーを持たせることで、ユーザーごとのデータを取得できるようにした

--- app/controllers/api/v1/ranks_controller.rb
+++ app/controllers/api/v1/ranks_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Api::V1::RanksController < ApplicationController
+  before_action :set_urls, only: [:index]
+  skip_before_action :authenticate_user!
+
   def index
-    @urls = Url.all
   end
+
+  private
+    def set_urls
+      if User.find_by(api_key: params[:api_key])
+        user = User.find_by(api_key: params[:api_key])
+        @urls = user.urls
+      end
+    end
 end
