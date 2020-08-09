15b34aa7955707a1e2fde804fa3b68eb4f486b95
horikoshi <mkhth3a@gmail.com>
2020-08-09 16:10:14 +0900

    find_rankメソッドを修正

--- app/models/rank_checker.rb
+++ app/models/rank_checker.rb
@@ -2,10 +2,9 @@
 
 class RankChecker
   def find_rank(urls, registered_url)
-    urls.each.with_index(1) do |url, index|
-      if url == registered_url || url == registered_url + "/"
-        return index
-      end
+    index = urls.index { |url| url == registered_url || url == registered_url + "/" }
+    if index
+      return index + 1
     end
     0
   end
