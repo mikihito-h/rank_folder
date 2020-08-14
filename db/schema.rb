a6bd610446a763a835d7182fa050c123ddbfbe09
horikoshi <mkhth3a@gmail.com>
2020-08-14 13:26:38 +0900

    keyword1つに付き、rankingが1日1つになるように複合ユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_023934) do
+ActiveRecord::Schema.define(version: 2020_08_14_042101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_023934) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.date "acquired_on", null: false
+    t.index ["keyword_id", "acquired_on"], name: "index_rankings_on_keyword_id_and_acquired_on", unique: true
     t.index ["keyword_id"], name: "index_rankings_on_keyword_id"
   end
 
