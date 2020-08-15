df96145dcbdf203ffefebb9ba762eecc53b43701
horikoshi <mkhth3a@gmail.com>
2020-08-15 17:58:25 +0900

    DRYにした

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -16,7 +16,6 @@ class Keyword < ApplicationRecord
 
   def create_rank
     urls =  fetch_urls
-
     if urls.present?
       rank = get_rank(urls, self.url.url)
     else
@@ -30,17 +29,21 @@ class Keyword < ApplicationRecord
 
   private
     def fetch_urls
-      response = request_to_google
-      if response.code == "200"
+      start_index = 1
+      urls = []
+      begin
+        response = request_to_google(start_index)
         json_response_body = JSON.parse(response.body)
-        urls = extract_urls(json_response_body)
-      else
-        Rails.logger.error "Google Custom Search APIのレスポンスがエラーでした。\nレスポンスコード: #{response.code}\nレスポンスボディー: #{response.body}"
-        return []
-      end
-      if json_response_body["queries"]["nextPage"]
-        urls += get_100_urls(json_response_body)
-      end
+
+        if response.code != "200"
+          Rails.logger.error "Google Custom Search APIのレスポンスがエラーでした。\nレスポンスコード: #{response.code}\nレスポンスボディー: #{response.body}"
+          break
+        elsif extract_urls(json_response_body) == []
+          break
+        end
+
+        urls += extract_urls(json_response_body)
+      end while (start_index = json_response_body.dig("queries", "nextPage", 0, "startIndex")) && start_index <= 91
       urls
     end
 
@@ -49,7 +52,7 @@ class Keyword < ApplicationRecord
       index ? index + 1 : 0
     end
 
-    def request_to_google(start_index = 1)
+    def request_to_google(start_index)
       query = URI.encode_www_form(key: ENV["GOOGLE_API_KEY"], cx: ENV["GOOGLE_CSE_ID"], q: self.keyword, safe: "off", num: 10, start: start_index)
       built_url = URI::HTTPS.build(host: "www.googleapis.com", path: "/customsearch/v1", query: query)
       Net::HTTP.get_response(built_url)
@@ -63,16 +66,5 @@ class Keyword < ApplicationRecord
       else
         []
       end
-    end
-
-    def get_100_urls(json_response_body)
-      urls = []
-      while (start_index = json_response_body.dig("queries", "nextPage", 0, "startIndex")) && start_index <= 91
-        response = request_to_google(start_index)
-        break unless response.code == "200"
-        json_response_body = JSON.parse(response.body)
-        urls += extract_urls(json_response_body)
-      end
-      urls
     end
 end
