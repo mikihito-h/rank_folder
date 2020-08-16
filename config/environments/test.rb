6c4a1c8e7fcabe542701d9978a3147d5f12fbe52
horikoshi <mkhth3a@gmail.com>
2020-08-16 16:57:52 +0900

    修正

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,5 +50,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # not default
-  config.action_mailer.default_url_options = { host: "localhost", port: 5432 }
+  config.action_mailer.default_url_options = { host: "localhost" }
 end
