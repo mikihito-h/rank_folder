832b30f7e9feb2ee54561195414f4ad9cd848b85
horikoshi <mkhth3a@gmail.com>
2020-08-22 17:56:34 +0900

    Keyword#create_rankメソッドのテストを追加

--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -2,8 +2,24 @@
 
 require "test_helper"
 
+VCR.configure do |config|
+  config.cassette_library_dir = Rails.root.join "test", "vcr_cassettes"
+  config.hook_into :webmock
+  config.ignore_localhost = true
+  config.allow_http_connections_when_no_cassette = false
+  config.filter_sensitive_data("{key}") { ENV.fetch("GOOGLE_API_KEY") }
+  config.filter_sensitive_data("{cse}") { ENV.fetch("GOOGLE_CSE_ID") }
+end
+
 class KeywordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    VCR.use_cassette("google_mock") do
+      @before_create_rank = Ranking.count
+      Keyword.find_by(keyword: "Ruby スクール").create_rank
+    end
+  end
+
+  test "#create_rank" do
+    assert_not_equal @before_create_rank, Ranking.count
+  end
 end
