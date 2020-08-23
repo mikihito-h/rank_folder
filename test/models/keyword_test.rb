8afcac01f26a279e6774c7dd127ec6f2189b7d50
horikoshi <mkhth3a@gmail.com>
2020-08-23 10:01:22 +0900

    vcrの設定内容を別のファイルに移動

--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -2,15 +2,6 @@
 
 require "test_helper"
 
-VCR.configure do |config|
-  config.cassette_library_dir = Rails.root.join "test", "vcr_cassettes"
-  config.hook_into :webmock
-  config.ignore_localhost = true
-  config.allow_http_connections_when_no_cassette = false
-  config.filter_sensitive_data("{key}") { ENV.fetch("GOOGLE_API_KEY") }
-  config.filter_sensitive_data("{cse}") { ENV.fetch("GOOGLE_CSE_ID") }
-end
-
 class KeywordTest < ActiveSupport::TestCase
   def setup
     VCR.use_cassette("google_mock") do
