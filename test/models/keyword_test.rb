3968d17b84e7577a7e127b0f6789cd66ba424179
horikoshi <mkhth3a@gmail.com>
2020-08-23 17:15:14 +0900

    vcrの設定を修正

--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -2,15 +2,6 @@
 
 require "test_helper"
 
-VCR.configure do |config|
-  config.cassette_library_dir = Rails.root.join "test", "vcr_cassettes"
-  config.hook_into :webmock
-  config.ignore_localhost = true
-  config.allow_http_connections_when_no_cassette = false
-  config.filter_sensitive_data("{key}") { ENV["GOOGLE_API_KEY"] }
-  config.filter_sensitive_data("{cse}") { ENV["GOOGLE_CSE_ID"] }
-end
-
 class KeywordTest < ActiveSupport::TestCase
   test "#create_rank" do
     @before_create_rank = Ranking.count
