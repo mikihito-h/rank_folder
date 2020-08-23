813f2d9ebdf31ea8d4405a8d72412d060226722d
horikoshi <mkhth3a@gmail.com>
2020-08-23 10:55:36 +0900

    テストを修正

--- test/models/keyword_test.rb
+++ test/models/keyword_test.rb
@@ -3,14 +3,11 @@
 require "test_helper"
 
 class KeywordTest < ActiveSupport::TestCase
-  def setup
+  test "#create_rank" do
+    @before_create_rank = Ranking.count
     VCR.use_cassette("google_mock") do
-      @before_create_rank = Ranking.count
       Keyword.find_by(keyword: "Ruby スクール").create_rank
     end
-  end
-
-  test "#create_rank" do
     assert_not_equal @before_create_rank, Ranking.count
   end
 end
