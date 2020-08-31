8bfbc016381dc3eb4330ddad7ad1c003ec0813b3
horikoshi <mkhth3a@gmail.com>
2020-08-31 11:57:58 +0900

    テストを修正

--- test/system/urls_test.rb
+++ test/system/urls_test.rb
@@ -6,15 +6,15 @@ class UrlsTest < ApplicationSystemTestCase
   test "URL一覧を表示する" do
     login_as users(:user_1)
     visit urls_path
-    assert_selector "h2", text: "URL一覧"
+    assert_selector "h2", text: "サイト一覧"
   end
 
   test "URLを新規登録する" do
     login_as users(:user_3)
     visit urls_path
-    click_on "URL追加"
+    click_on "サイトを追加"
 
-    fill_in "URL", with: "https://example.com"
+    fill_in "サイトURL", with: "https://example.com"
     click_on "検索ワードを追加"
     fill_in "検索ワード", with: "ruby スクール"
     click_on "検索ワードを追加"
@@ -26,18 +26,18 @@ class UrlsTest < ApplicationSystemTestCase
   test "URL詳細画面を表示する" do
     login_as users(:user_1)
     visit urls_path
-    click_on "詳細", match: :first
-    assert_selector "h2", text: "URL詳細"
+    click_on "順位を見る", match: :first
+    assert_selector "h2", text: "サイトURL"
   end
 
   test "URLを削除" do
     login_as users(:user_1)
     visit urls_path
-    click_on "詳細", match: :first
+    click_on "順位を見る", match: :first
 
     assert_difference "Url.count", -1 do
       page.accept_confirm do
-        click_on "URL削除"
+        click_on "URLを削除"
       end
       assert_text "Urlを削除しました。"
     end
@@ -46,7 +46,7 @@ class UrlsTest < ApplicationSystemTestCase
   test "検索ワードを削除" do
     login_as users(:user_1)
     visit urls_path
-    click_on "詳細", match: :first
+    click_on "順位を見る", match: :first
 
     assert_difference "Keyword.count", -1 do
       page.accept_confirm do
@@ -59,8 +59,8 @@ class UrlsTest < ApplicationSystemTestCase
   test "検索ワードを追加" do
     login_as users(:user_3)
     visit urls_path
-    click_on "詳細", match: :first
-    click_on "検索ワード追加"
+    click_on "順位を見る", match: :first
+    click_on "検索ワードを追加"
 
     assert_difference "Keyword.count", 1 do
       fill_in "検索ワード", with: "表参道 ランチ"
