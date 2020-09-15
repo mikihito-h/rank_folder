ae7a5044c41bf145a990221c795b4284d7fed21c
horikoshi <mkhth3a@gmail.com>
2020-09-15 16:19:39 +0900

    不要な空白を削除

--- db/migrate/20200912075603_add_index_to_user.rb
+++ db/migrate/20200912075603_add_index_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddIndexToUser < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token, unique: true
   end
 end
